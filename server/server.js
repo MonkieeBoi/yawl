0f52419a94cb7add07fdff65880da2faec22620b
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-19 21:40:50 +1100

    chore: init express project structure

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const port = process.env.SERVER_PORT || 7000;
+const app = express();
+
+app.use(express.json()); //middle ware to accept json data in req.body
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
+app.get("/heart", (req, res) => {
+  res.send("Server is healthy");
+})
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`server started at https://localhost:${port}`);
+});
