788fd2f02491fd43cb584bcdad8b01ce02245931
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-24 16:32:20 +1100

    fix: added wishlistroute to server

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import swaggerDocs from './utils/swagger.js';
 import itemRoute from "./routes/item.route.js"
+import wishlistRoute from "./routes/wishlist.route.js"
 
 const port = process.env.SERVER_PORT || 7000;
 const app = express();
 
 app.use(express.json()); //middle ware to accept json data in req.body
 app.use("/api/item", itemRoute);
+app.use("/api/wishlist", wishlistRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
